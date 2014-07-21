9914013eba1a97cd02e71883410ecef941f9d59b
Susan Brown <brownsusan@fullsail.edu>
2014-07-21 15:45:37 -0400

    Adds  responsive background image

--- js/core/main.js
+++ js/core/main.js
@@ -1,2 +1,19 @@
 // TODO
 // Input padding with javascript
+
+$(document).ready(function() {
+	moveDescription();
+
+});
+
+$(window).resize(function() {
+	moveDescription();
+});
+
+
+var moveDescription = function() {
+	//Need to reset css here when the window is scaled back up past the media queries
+	var bgImgHt = $('#main-feature').css('height');
+	$('.degree-description').css('margin-top', bgImgHt);
+
+};
