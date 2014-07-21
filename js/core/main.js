f81dce89c9493fd8280605b8324bde1f586ac36d
Susan Brown <brownsusan@fullsail.edu>
2014-07-21 19:18:27 -0400

    Adds slider, still has sizing issue

--- js/core/main.js
+++ js/core/main.js
@@ -3,13 +3,28 @@
 
 $(document).ready(function() {
 	moveContent();
-	
+
 	var headerHeight = $('header').height(), logoHeight = $('header img').height();
-	var logoMargin = (headerHeight - logoHeight)/2;
+	var logoMargin = (headerHeight - logoHeight) / 2;
 	$('header img').css('margin-top', logoMargin);
-	
+
 	var selectHeight = $('select').height();
 	$('select').css('background-size', selectHeight);
+
+	slidr.create('slidr-ul', {
+		breadcrumbs : true,
+		controls : 'corner',
+		direction : 'horizontal',
+		fade : false,
+		keyboard : true,
+		overflow : true,
+		pause : false,
+		timing : {
+			'cube' : '0.5s ease-in'
+		},
+		touch : true,
+		transition : 'cube'
+	}).start();
 });
 
 $(window).resize(function() {
@@ -20,6 +35,6 @@ var moveContent = function() {
 	//Need to reset css here when the window is scaled back up past the media queries
 	var height = $('#main-feature').css('height');
 	var bgImgHt = parseInt(height);
-	$('.degree-description').css('margin-top', bgImgHt+30);
-	$('.degree-name').css('position', 'absolute').css('top', bgImgHt-25-$('.degree-name').height());
+	$('.degree-description').css('margin-top', bgImgHt + 30);
+	$('.degree-name').css('position', 'absolute').css('top', bgImgHt - 25 - $('.degree-name').height());
 };
